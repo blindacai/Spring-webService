616566882036f4be36dd0fd106bd320e9cf2b765
Christine <xtinesam@gmail.com>
2017-03-28 15:26:07 -0700

    rounding double

--- src/main/Java/Tables/Rating.java
+++ src/main/Java/Tables/Rating.java
@@ -2,6 +2,7 @@ package Tables;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import static jdk.nashorn.internal.objects.NativeMath.round;
 
 /**
  * Created by csam on 2017-03-28.
@@ -10,7 +11,7 @@ public class Rating {
     private double rating;
 
     public Rating(ResultSet result) throws SQLException {
-        this.rating = result.getDouble("rating");
+        this.rating = round(result.getDouble("rating"), 1);
     }
 
     public double getRating() {
