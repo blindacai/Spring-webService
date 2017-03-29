6519967e60323088c1290ea70a3c787a63a1bd44
Christine <xtinesam@gmail.com>
2017-03-29 10:36:43 -0700

    removed on cascade delete for reviews

--- all.sql
+++ all.sql
@@ -76,7 +76,7 @@ create table review (
 	title varchar(50) not null, 
 	releasedate varchar(10) not null, 
 	foreign key (accountname) references users on delete cascade, 
-	foreign key (title, releasedate) references movie on delete cascade 
+	foreign key (title, releasedate) references movie 
 );
 create table upvote ( 
 	accountname varchar(25) not null, 
@@ -96,6 +96,7 @@ insert into movie values('Mona Lisa Smile', '2003-12-19', 'Mike Newell', 'Columb
 insert into movie values('Saving Private Ryan', '1998-07-24', 'Steven Spielberg', 'DreamWorks Pictures');
 insert into movie values('Harry Potter', '2001-11-04', 'Chris Columbus', 'Warner Bros. Pictures');
 insert into movie values('Coming Home', '2014-05-16', 'Zhang Yimou', 'Sony Pictures Classics');
+insert into movie values('Beauty and the Beast', '2017-02-23', 'Bill Condon', 'Walt Disney Studios');
 -- insert into actor
 insert into actor values('Tom Hanks', '1956-07-09', 'American');
 insert into actor values('Gong Li', '1965-12-31', 'Singaporean');
@@ -131,6 +132,7 @@ insert into comments values('omg', '2004-01-25', 2, seqC.NEXTVAL, 'gameofthreads
 insert into comments values('hehehe', '1998-08-13', 1, seqC.NEXTVAL, 'helloworld', 'Saving Private Ryan', '1998-07-24');
 insert into comments values('what a movie', '2016-01-10', 0, seqC.NEXTVAL, 'helloworld', 'Coming Home', '2014-05-16');
 insert into comments values('what a movie two', '2016-01-12', 3, seqC.NEXTVAL, 'userone', 'Coming Home', '2014-05-16');
+insert into comments values('My favourite movie', '2017-03-23', 1, seqC.NEXTVAL, 'helloworld', 'Beauty and the Beast', '2017-02-23');
 -- insert into review
 insert into review values ('The actors did a phenomenal job.', '2000-10-30', 4, seqR.NEXTVAL, 'helloworld', 'Remember the Titans', '2000-09-23');
 insert into review values ('I didnt think I would like a movie about football, but it was amazing!', '2001-02-16', 3, seqR.NEXTVAL, 'foobar', 'Remember the Titans', '2000-09-23');
